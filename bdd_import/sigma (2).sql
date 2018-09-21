964381dda792e28db3d9855285bcde34b68bf618
zakaria khaine <zskparis@gmail.com>
2018-09-21 11:40:13 +0200

    cy

---
+++ bdd_import/sigma (2).sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 21 sep. 2018 à 09:39
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.0.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `sigma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tbl_image`
+--
+
+DROP TABLE IF EXISTS `tbl_image`;
+CREATE TABLE IF NOT EXISTS `tbl_image` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_name` varchar(250) NOT NULL,
+  `image_description` varchar(250) NOT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <body>
 
-<a href="test_CRUD/index.php">FICHIER AJOUTEey</a>
+<a href="test_CRUD/index.php">NTM NMN</a>
 
 </body>
 
