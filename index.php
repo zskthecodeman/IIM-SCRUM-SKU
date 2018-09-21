d62a0d13b639505f7b7e687827f208142e39f739
zakaria khaine <zskparis@gmail.com>
2018-09-21 11:33:29 +0200

    index

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Zakaria Khaine">
+    <![endif]-->
+</head>
+
+<body>
+
+<a href="test_CRUD/index.php">FICHIER AJOUTER</a>
+
+</body>
+
+</html>
